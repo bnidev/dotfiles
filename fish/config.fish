2fbfa9c3fc06fbe0da202f69df1dce4a576e18ca
Benjamin Diener <bd@benjamindiener.com>
2024-08-25 21:50:22 +0200

    feat(fish): move exports to env.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,11 @@
 if status is-interactive
     set fish_greeting # remove fish's greeting
 
-    set parts alias functions
+    set parts alias functions env
     for part in $parts
       set file $HOME/.config/fish/parts/$part.fish
       if test -e $file
         source $file
       end
     end
-
-    export LS_COLORS="ow=34" # fix highlight color for directory writable to others (ow)
 end
---
+++ fish/parts/env.fish
@@ -0,0 +1,5 @@
+# make vscode the default editor
+export EDITOR="code --wait"
+
+# fix highlight color for directory writable to others (ow)
+export LS_COLORS="ow=34"
