ccc3a359e96efb855b04b72e755bc410e702ba5a
Benjamin Diener <bd@benjamindiener.com>
2024-08-26 22:31:17 +0200

    feat(fish): add eza alias and abbreviations

--- fish/parts/alias.fish
+++ fish/parts/alias.fish
@@ -22,3 +22,14 @@ abbr . 'nautilus .'
 abbr ip "dig +short myip.opendns.com @resolver1.opendns.com"
 abbr localip "ipconfig getifaddr en0"
 abbr ips "ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# ls
+if type -q eza
+  set TREE_IGNORE 'cache|log|logs|node_modules|vendor|.git'
+  alias ls 'eza --icons'
+  abbr ls 'ls -a'
+  abbr ll 'ls -la'
+  abbr lt 'ls --tree -D -L 2 -I $TREE_IGNORE'
+  abbr ltt 'ls --tree -D -L 3 -I $TREE_IGNORE'
+  abbr lttt 'ls --tree -D -L 4 -I $TREE_IGNORE'
+end
