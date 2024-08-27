24b7970d90d7917a96fe8b178fda6a0af49e8c29
Benjamin Diener <bd@benjamindiener.com>
2024-08-27 22:26:01 +0200

    feat(starship): add starship config

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,13 @@
 if status is-interactive
-    set fish_greeting # remove fish's greeting
+  set fish_greeting # remove fish's greeting
 
-    set parts alias functions env
-    for part in $parts
-      set file $HOME/.config/fish/parts/$part.fish
-      if test -e $file
-        source $file
-      end
+  set parts alias functions env
+  for part in $parts
+    set file $HOME/.config/fish/parts/$part.fish
+    if test -e $file
+      source $file
     end
+  end
+
+  starship init fish | source
 end
