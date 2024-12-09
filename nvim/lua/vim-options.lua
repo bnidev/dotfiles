65cbf2682ff4d94158377b91947e6559d6ccfe4c
bnidev <bd@benjamindiener.com>
2024-12-09 22:11:19 +0100

    feat(nvim): add configuration files

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,7 @@
+vim.opt.number = true        -- Show line numbers
+vim.opt.relativenumber = false -- Relative line numbers
+vim.opt.tabstop = 2          -- Set tab width to 2 spaces
+vim.opt.shiftwidth = 2       -- Set shift width to 2 spaces
+vim.opt.expandtab = true     -- Use spaces instead of tabs
+vim.opt.smartindent = true   -- Smart indentation
+vim.g.mapleader= " "
