91cf45e79a38b359603c2b671b599bf497b7af67
bnidev <bd@benjamindiener.com>
2024-12-11 22:26:51 +0100

    feat(nvim): enhance language support

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls" },
+				ensure_installed = { "lua_ls", "ts_ls", "html", "tailwindcss", "pyright", "volar"},
 			})
 		end,
 	},
@@ -21,9 +21,24 @@ return {
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities
+      })
 			lspconfig.html.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        init_options = {
+          vue = {
+            hybridMode = false,
+          },
+          typescript = {
+            tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+          },
+        },
+      })
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "javascript", "typescript", "html", "css", "vue", "json", "php" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
