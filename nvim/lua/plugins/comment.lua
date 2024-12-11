17dfe7df958f37a1e2eeaf4139bae636d55a2541
bnidev <bd@benjamindiener.com>
2024-12-11 22:25:16 +0100

    feat(nvim): add comment and surround plugins

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,14 @@
+return {
+	"numToStr/Comment.nvim",
+	opts = {
+		-- add any options here
+	},
+	config = function()
+		require("Comment").setup({
+			toggler = {
+				line = "gcc",
+        block = "gbc"
+			},
+		})
+	end,
+}
