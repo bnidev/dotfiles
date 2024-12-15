4772863025c630d62137c26cbea652af12a5be46
bnidev <bd@benjamindiener.com>
2024-12-15 18:16:41 +0100

    feat(fish): add fzf preview settings

--- fish/config.fish
+++ fish/config.fish
@@ -13,4 +13,13 @@ if status is-interactive
   if command -v 'starship' &>/dev/null
     starship init fish | source
   end
+
+  # FZF
+  fzf --fish | source
+
+  export FZF_DEFAULT_OPTS="--height 50% --layout=default --border --color=hl:#2dd4bf"
+
+  export FZF_CTRL_T_OPTS="--preview 'bat --color=always -n --line-range :500 {}'"
+  export FZF_ALT_C_OPTS="--preview 'eza --icons=always --tree --color=always {} | head -200'"
+  export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
 end
