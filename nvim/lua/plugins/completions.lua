b97daaa856292f8e5d8620af0b2208dec95d2bc2
bnidev <bd@benjamindiener.com>
2024-12-16 22:04:22 +0100

    feat(nvim): add codeium, nvim-ts-autotag, oil and snacks plugins

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -34,6 +34,7 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
+          { name = "codeium" }
         }, {
           { name = "buffer" },
         }),
