af1c7824d6a9769ea2a9e0404db03f15ce6583b1
Benjamin Diener <benni@mybenni.de>
2025-01-14 22:07:36 +0100

    feat(nvim): add which-key plugin and descriptions for most keymaps

--- roles/neovim/files/lua/keymaps.lua
+++ roles/neovim/files/lua/keymaps.lua
@@ -5,23 +5,24 @@ vim.g.mapleader = " "
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
 -- buffers
-vim.keymap.set("n", "<leader>n", ":bn<CR>")
-vim.keymap.set("n", "<leader>p", ":bp<CR>")
-vim.keymap.set("n", "<leader>c", ":bd<CR>")
+vim.keymap.set("n", "<leader>n", ":bn<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>p", ":bp<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>c", ":bd<CR>", { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>", { desc = "Pick buffer" })
 
 -- move lines
 vim.keymap.set({ "n", "v" }, "<A-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set({ "n", "v" }, "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- rename
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
 
 -- Obsidian
-vim.keymap.set("n", "<leader>on", ":ObsidianTemplate<CR>")
-vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
+vim.keymap.set("n", "<leader>on", ":ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
+vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>", { desc = "Format Obsidian Headline" })
 
 -- Markdown Preview
-vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<CR>")
+vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<CR>", { desc = "Markdown Preview" })
 
 -- Todo Comments
-vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>")
+vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>", { desc = "Find Todo Comments" })
