e89d911df32c5804784a7694ac3f6bd4f19aaa9b
Benjamin Diener <benni@mybenni.de>
2025-05-11 20:03:04 +0200

    feat(nvim): add mini.trailspace and and mini.splitjoin modules

--- roles/neovim/files/lua/plugins/mini.lua
+++ roles/neovim/files/lua/plugins/mini.lua
@@ -1,8 +1,17 @@
 return {
   "echasnovski/mini.nvim",
+  event = "BufRead",
   version = false,
   config = function()
     require("mini.surround").setup()
     require("mini.operators").setup()
+    require("mini.trailspace").setup({
+      only_in_normal_buffers = true
+    })
+
+    vim.keymap.set("n", "<leader>cw", function()
+      require("mini.trailspace").trim()
+    end, { desc = "Trim trailing whitespace" })
+    require("mini.splitjoin").setup({})
   end,
 }
