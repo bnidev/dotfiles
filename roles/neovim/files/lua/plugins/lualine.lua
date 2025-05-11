6787bc04a998aa20577b20adda0bbe231adecc5c
Benjamin Diener <benni@mybenni.de>
2025-05-11 20:10:15 +0200

    feat(nvim): add colorized git diff component to lualine

--- roles/neovim/files/lua/plugins/lualine.lua
+++ roles/neovim/files/lua/plugins/lualine.lua
@@ -1,11 +1,29 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
-        theme = 'material'
-      }
+        theme = "material",
+      },
+      sections = {
+        lualine_a = {
+          {
+            "diff",
+            colored = true,
+            diff_color = {
+              added = "LuaLineDiffAdd",
+              modified = "LuaLineDiffChange",
+              removed = "LuaLineDiffDelete",
+            },
+            symbols = { added = "+", modified = "~", removed = "-" },
+            source = nil, -- A function that works as a data source for diff.
+            -- It must return a table as such:
+            --   { added = add_count, modified = modified_count, removed = removed_count }
+            -- or nil on failure. count <= 0 won't be displayed.
+          },
+        },
+      },
     })
-  end
+  end,
 }
