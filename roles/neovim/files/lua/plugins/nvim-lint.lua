b813c23b08e1e0462a7f34a106fc86284dcc6812
Benjamin Diener <benni@mybenni.de>
2025-05-11 20:59:05 +0200

    fix(nvim): make sure eslint_d works when installed by Mason

--- roles/neovim/files/lua/plugins/nvim-lint.lua
+++ roles/neovim/files/lua/plugins/nvim-lint.lua
@@ -7,6 +7,18 @@ return {
   config = function()
     local lint = require("lint")
 
+    -- Override eslint_d config for `nvim-lint`
+    lint.linters.eslint_d = {
+      cmd = vim.fn.stdpath("data") .. "/mason/bin/eslint_d", -- Path to eslint_d installed by Mason
+      stdin = false, -- Don't use stdin (i.e., no live feedback)
+      args = {}, -- Don't pass deprecated flags like --extensions or --useEslintrc
+      stream = "stdout", -- Stream lint results to stdout
+      ignore_exitcode = true, -- Ignore non-zero exit codes from eslint_d
+      parser = require("lint.parser").from_errorformat("%f: line %l, col %c, %trror - %m", {
+        source = "eslint_d",
+        severity = vim.diagnostic.severity.ERROR, -- Set severity to error for ESLint issues
+      }),
+    }
 
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
