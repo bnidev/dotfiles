06ece675a37913dadd6c4658347ac52b62bbcd61
Benjamin Diener <benni@mybenni.de>
2025-05-11 20:06:38 +0200

    feat(nvim): enable auto-focus when Trouble is opened

--- roles/neovim/files/lua/plugins/trouble.lua
+++ roles/neovim/files/lua/plugins/trouble.lua
@@ -1,6 +1,8 @@
 return {
   "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {
+    focus = true
+  },
   cmd = "Trouble",
   keys = {
     {
