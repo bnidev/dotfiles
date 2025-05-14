9748fadc8dc0718ee2e4bea94b34904493f6e175
Benjamin Diener <benni@mybenni.de>
2025-05-14 21:41:38 +0200

    feat(nvim): include hidden files in telescope file picker

--- roles/neovim/files/lua/plugins/telescope.lua
+++ roles/neovim/files/lua/plugins/telescope.lua
@@ -43,7 +43,7 @@ return {
 			require("telescope").load_extension("ui-select")
 
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<C-p>", function() builtin.find_files({ hidden = true }) end, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fb", builtin.current_buffer_fuzzy_find, {})
