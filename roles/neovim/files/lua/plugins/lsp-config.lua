17c1d72554e4c1ee5316596fc80851a421474d2c
Benjamin Diener <benni@mybenni.de>
2025-07-06 20:03:20 +0200

    feat(nvim): add tiny-inline-diagnostic plugin to improve inline diagnostic display

--- roles/neovim/files/lua/plugins/lsp-config.lua
+++ roles/neovim/files/lua/plugins/lsp-config.lua
@@ -152,8 +152,24 @@ return {
               unusedparams = true,
             },
           },
-        }
+        },
       })
+    end,
+  },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup({
+        preset = "modern",
+        options = {
+          show_all_diags_on_cursorline = true,
+          use_icons_from_diagnostic = true,
+          enable_on_select = true,
+        },
+      })
+      vim.diagnostic.config({ virtual_text = false })
     end,
   },
 }
