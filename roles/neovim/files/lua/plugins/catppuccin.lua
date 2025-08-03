56d4abfafab88a8e5fc6b440b9104347dedcc048
Benjamin Diener <benni@mybenni.de>
2025-08-03 14:48:13 +0200

    feat(nvim): add custom highlights for better visuals when using flash.nvim

--- roles/neovim/files/lua/plugins/catppuccin.lua
+++ roles/neovim/files/lua/plugins/catppuccin.lua
@@ -8,8 +8,23 @@ return {
       require("catppuccin").setup({
         -- flavour = "macchiato"
         -- transparent_background = true,
+        integrations = {
+          flash = true, -- enable flash.nvim support
+        },
+
+        highlight_overrides = {
+          all = function(colors)
+            return {
+              FlashBackdrop = { fg = "#545c7e" },
+              FlashCurrent = { bg = "#ff966c", fg = "#1b1d2b" },
+              FlashLabel = { bg = "#ff007c", fg = "#c8d3f5", bold = true },
+              FlashMatch = { bg = "#3e68d7", fg = "#c8d3f5" },
+              FlashCursor = { reverse = true },
+            }
+          end,
+        },
       })
-      vim.cmd.colorscheme "catppuccin"
-    end
-  }
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
