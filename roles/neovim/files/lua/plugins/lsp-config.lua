b5921f49afde66757df99faeb0903e42fa606516
Benjamin Diener <benni@mybenni.de>
2025-10-04 17:48:30 +0200

    chore(nvim): modernize LSP config for Neovim 0.11 specs

--- roles/neovim/files/lua/plugins/lsp-config.lua
+++ roles/neovim/files/lua/plugins/lsp-config.lua
@@ -9,23 +9,8 @@ return {
     "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        automatic_enable = false,
-        ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "html",
-          "tailwindcss",
-          "pyright",
-          "volar",
-          "cssls",
-          "yamlls",
-          "jsonls",
-          "gopls",
-          "dockerls",
-          "docker_compose_language_service",
-          "intelephense",
-          "laravel_ls",
-        },
+        automatic_enable = true,
+        ensure_installed = {}, -- added via mason-tool-installer
       })
     end,
   },
@@ -39,6 +24,20 @@ return {
           { "eslint_d", version = "13.1.2" },
           "biome",
           "php-cs-fixer",
+          "cssls",
+          "html",
+          "lua_ls",
+          "tailwindcss",
+          "pyright",
+          "yamlls",
+          "jsonls",
+          "gopls",
+          "dockerls",
+          "docker_compose_language_service",
+          "intelephense",
+          "laravel_ls",
+          "volar",
+          "ts_ls",
         },
       })
     end,
@@ -47,34 +46,10 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
-       { "folke/neodev.nvim", opts = {} },
+      { "folke/neodev.nvim", opts = {} },
     },
     config = function()
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Dislay hover info" })
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-      vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, { desc = "Go to references" })
-      vim.keymap.set(
-        "n",
-        "<leader>ds",
-        require("telescope.builtin").lsp_document_symbols,
-        { desc = "Show documen symbols" }
-      )
-      vim.keymap.set(
-        "n",
-          "<leader>ws",
-          require("telescope.builtin").lsp_dynamic_workspace_symbols,
-          { desc = "Show workspace symbols" }
-      )
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
-      vim.keymap.set("n", "<A-n>", function()
-        vim.diagnostic.jump({ count = 1 })
-      end, { desc = "Next diagnostic" })
-      vim.keymap.set("n", "<A-p>", function()
-        vim.diagnostic.jump({ count = -1 })
-      end, { desc = "Previous diagnostic" })
-      vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, { desc = "Open diagnostic" })
-
-      -- Define sign icons for each severity
+      -- Diagnostic signs
       local signs = {
         [vim.diagnostic.severity.ERROR] = " ",
         [vim.diagnostic.severity.WARN] = " ",
@@ -82,89 +57,113 @@ return {
         [vim.diagnostic.severity.INFO] = " ",
       }
 
-      -- Set the diagnostic config with all icons
       vim.diagnostic.config({
-        signs = {
-          text = signs,
-        },
+        signs = { text = signs },
         virtual_text = true,
         underline = true,
         update_in_insert = false,
       })
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig/util")
       require("neodev").setup()
 
-      lspconfig.ts_ls.setup({
+      -- Default config for all servers unless overridden
+      vim.lsp.config("*", {
         capabilities = capabilities,
+        root_markers = { ".git" },
       })
 
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
+      local function setup_lsp_servers()
 
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
 
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.volar.setup({
-        capabilities = capabilities,
-        filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-      })
-
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-        settings = {
-          yaml = {
-            schemaStore = {
-              url = "https://www.schemastore.org/api/json/catalog.json",
-              enable = true,
+        local servers = {
+          tsserver = {},
+          tailwindcss = {},
+          html = {},
+          cssls = {},
+          volar = {
+            filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+          },
+          lua_ls = {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  enable = true,
+                  globals = { "vim" },
+                },
+                workspace = { checkThirdParty = false },
+                telemetry = { enable = false },
+              },
             },
           },
-        },
-      })
-
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        cmd = { "gopls" },
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-        settings = {
+          pyright = {},
+          yamlls = {
+            settings = {
+              yaml = {
+                schemaStore = {
+                  url = "https://www.schemastore.org/api/json/catalog.json",
+                  enable = true,
+                },
+              },
+            },
+          },
+          jsonls = {},
           gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            analyses = {
-              unusedparams = true,
+            cmd = { "gopls" },
+            filetypes = { "go", "gomod", "gowork", "gotmpl" },
+            root_dir = vim.fs.root(0, { "go.work", "go.mod", ".git" }),
+            settings = {
+              gopls = {
+                completeUnimported = true,
+                usePlaceholders = true,
+                analyses = {
+                  unusedparams = true,
+                },
+              },
             },
           },
-        },
+          intelephense = {},
+          laravel_ls = {
+            filetypes = { "php", "blade" },
+          },
+        }
+
+        for name, config in pairs(servers) do
+          config.capabilities = vim.tbl_deep_extend("force", {}, capabilities, config.capabilities or {})
+          vim.lsp.config(name, config)
+        end
+      end
+
+      -- Setup after mason-tool-installer completes
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MasonToolsInstallerCompleted",
+        callback = function()
+          setup_lsp_servers()
+        end,
       })
 
-      lspconfig.intelephense.setup({
-        capabilities = capabilities,
-      })
+      -- Also run immediately in case tools are already installed
+      setup_lsp_servers()
 
-      lspconfig.laravel_ls.setup({
-        capabilities = capabilities,
-        filetypes = { "php", "blade" },
+      -- Buffer-local keymaps on LspAttach
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local opts = { buffer = bufnr }
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, opts)
+          vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, opts)
+          vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<A-n>", function()
+            vim.diagnostic.jump({ count = 1 })
+          end, opts)
+          vim.keymap.set("n", "<A-p>", function()
+            vim.diagnostic.jump({ count = -1 })
+          end, opts)
+          vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, opts)
+        end,
       })
     end,
   },
